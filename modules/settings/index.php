945e912b4a71a009581aeeb868ca5ee7fb3137e3
Руслан <legostaevr@gmail.com>
2014-09-22 18:00:14 +0400

    start settings

---
+++ modules/settings/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Руслан
+ * Date: 22.09.14
+ * Time: 17:51
+ */
+?>
---
+++ modules/settings/modules/index.tpl
@@ -0,0 +1,5 @@
+<h1>Модули</h1>
+<a href="<?=$siteurl;?>settings/add">Добавить модуль</a>
+<table>
+
+</table>
