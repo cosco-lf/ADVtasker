dbcd440ff73a36956244e76571d32bfff30fa342
tigresse <alena624@mail.ru>
2014-09-23 12:48:13 +0400

    add parent modules

--- modules/settings/modules/add.php
+++ modules/settings/modules/add.php
@@ -1,8 +1,15 @@
 <?php
 
+$res = mysql_query("SELECT id_module as id, name as caption FROM ".$db['prefix']."modules");
+while ($row=@mysql_fetch_assoc($res)) {
+    $result[]=$row;
+
+}
+
 $array = array();
 array_push($array,addLine('text','module_name','Название'));
 array_push($array,addLine('text','module_ident','Идентификатор'));
+array_push($array,addLine('select','module_parent','Родительский модуль','','','',$result));
 array_push($array,addLine('button','','Отправить'));
 $form = generateForm($array);
 
--- modules/settings/modules/add_save.php
+++ modules/settings/modules/add_save.php
@@ -3,8 +3,9 @@ if (isset($_POST['save'])) {
 
     $module_name = trim(@$_POST['module_name']);
     $module_ident = trim(@$_POST['module_ident']);
+    $module_parent = trim(@$_POST['module_parent']);
 
-        mysql_query("INSERT INTO ".$db['prefix']."modules (name,ident) values ('".sql_quote($module_name)."','".sql_quote($module_ident)."')");
+        mysql_query("INSERT INTO ".$db['prefix']."modules (name,ident,id_parent_module) values ('".sql_quote($module_name)."','".sql_quote($module_ident)."','".sql_quote($module_parent)."')");
         if (mysql_error()) {
             $errors[] = 'sql';
         } else {
